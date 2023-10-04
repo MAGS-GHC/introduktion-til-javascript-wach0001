debbfad2157c7e1b109257d13bab388d3cbc2b66
wach0001 <wach0001@edu.mercantec.dk>
2023-10-04 14:05:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,73 @@ console.log("**")
 console.log("***")
 console.log("**")
 console.log("*")
+const goldenratio = 1.618
+let helheden
+helheden = 10 
+//styk == helheden / goldenratio
+console.log(typeof goldenratio)
+console.log(10-5)
+console.log(25/3)
+console.log(25%3)
+console.log(25%2)
+console.log(3**2)
+let tekst1 = "Hello"
+let tekst2 = "World!"
+console.log(tekst1 + tekst2)
+let gf2Online = "GF2 Online"
+console.log(gf2Online.length)
+let tilfældigtTal = (Math.random()*10)
+console.log(tilfældigtTal)
+let number = 23
+
+console.log(number)
+console.log(number = "23")
+console.log(number == "23")
+console.log(number === "23")
+console.log(number -= 23)
+console.log(number += 23)
+console.log(number += "23")
+console.log(number -= "46")
+console.log(number != "23")
+console.log(number **=2)
+let tal = 200;
+
+if(tal % 2==0){   
+console.log(" det her tal er lig");  //The number is even}
+}else {
+console.log ("Det her tal er ulig");  //The number is odd}
+}
+
+let tal2 = 27;
+
+
+if(tal2 % 5==0){
+  console.log(" går op i")  //The number is even}
+}else  {
+    console.log ("går ikke op i")  //The number is odd} 
+  }
+
+  let randomTal = (Math.random()*10)
+  
+
+  if(randomTal % 5==0){console.log("går op i") //The number is even
+}
+
+
+
+
+   
+  
+     
+  
+
+
+
+
+
+
+
+
 
 
 
