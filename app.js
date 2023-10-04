05ac4345978007a36ce9a2a0ce6d7bb9038ce7bc
wach0001 <wach0001@edu.mercantec.dk>
2023-10-04 19:20:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ console.log("***")
 console.log("**")
 console.log("*")
 const goldenratio = 1.618
+
+
 let helheden
 helheden = 10 
 //styk == helheden / goldenratio
@@ -22,9 +24,10 @@ console.log(25/3)
 console.log(25%3)
 console.log(25%2)
 console.log(3**2)
-let tekst1 = "Hello"
-let tekst2 = "World!"
-console.log(tekst1 + tekst2)
+let tekst1 = "Hello";
+let tekst2 = " World!";
+console.log(tekst1 + tekst2);
+
 let gf2Online = "GF2 Online"
 console.log(gf2Online.length)
 let tilfældigtTal = (Math.random()*10)
@@ -58,16 +61,67 @@ if(tal2 % 5==0){
     console.log ("går ikke op i")  //The number is odd} 
   }
 
-  let randomTal = (Math.random()*10)
-  
+  let randomTal = Math.floor(Math.random()*100+1);
+console.log(randomTal)
+
+
 
   if(randomTal % 5==0){console.log("går op i") //The number is even
+}else {console.log ("det går ikke op i") //The number is odd
+}
+
+
+
+let a = 25;
+let b = 33;
+let c = 12; 
+if (a > b && a > c){
+  console.log("A er størst");
+} else if (b > a && b > c) 
+{console.log("B er størst");}
+else {console.log("C er størst");}
+
+let temp = 24;
+if (temp <= 0){
+  console.log("Det er frostvejr");
+} else if (temp >= 1 && temp <= 10){
+  console.log("Det er meget koldt");
+  } else if (temp >= 11 && temp <= 20){
+    console.log("Det er koldt");
+  } else if (temp >= 21 && temp <= 30){
+    console.log("Det er normalt vejr");
+  } else if (temp >= 31 && temp <= 40){
+    console.log("Det er varmt");
+  } else if (temp >= 41){
+    console.log("Det er meget varmt");
+  }
+
+let dag =3;
+if (dag ==1){
+  console.log("Mandag");
+} else if (dag == 2){
+  console.log("Tirsdag");
+} else if (dag == 3){
+  console.log("Onsdag");
+} else if (dag == 4){
+  console.log("Torsdag");
+} else if (dag == 5){
+  console.log("Fredag");
+} else if (dag == 6){
+  console.log("Lørdag");
+} else if (dag == 7){
+  console.log("Søndag");
+
+  
 }
 
 
 
 
-   
+
+  
+
+
   
      
   
